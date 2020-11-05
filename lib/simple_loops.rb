7817ee167a91a66795cd933e4e8702037bc67d4e
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-05 04:15:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,14 +1,17 @@
 # Write your methods here
 def loop_message_five_times(message)
- count = 0 
- while count < 5
- puts message 
- count += 1 
- end
+  count = 0 
+  while count < 5
+  puts message 
+  count += 1 
+  end
 end
 
 def loop_message_n_times
-  
+  while count < n
+  puts message 
+  count += 1 
+  end
 end
 
 def output_array
